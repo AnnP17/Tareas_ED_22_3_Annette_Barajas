43b32536f2c77746701b5efe9019027c6f832d88
AnnP17 <106794689+AnnP17@users.noreply.github.com>
2022-06-05 19:51:44 -0500

    Tarea de Quizz Completada y correcta

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -5,31 +5,32 @@ using TMPro;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public TextMeshProUGUI texto;
   // Start is called before the first frame update
     void Start()
     {
-        texto.text = 'Ingrese Opcion';
+        texto.text = "Capitán de división de Toman favorito:   a) Mitsuya b) Baji c) Pahchin d) Smiley";
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            text.text= 'Mitsuya: No odies la situación en la que naciste';
+            texto.text= "Mitsuya: No odies la situación en la que naciste";
         }
-        if (input.GetKeyDown(KeyCode.B))
+        if (Input.GetKeyDown(KeyCode.B))
         {
-            text.text= 'Baji: Uno para todos, y todos para uno. Pondremos nuestras vidas en juego por los otros'
+            texto.text= "Baji: Uno para todos, y todos para uno. Pondremos nuestras vidas en juego por los otros";
         }
-        if (input.GetKeyDown(KeyCode.C))
+        if (Input.GetKeyDown(KeyCode.C))
         {
-            text.text= 'Pahchin: EH?! No sé nada sobre esa tontería porque soy demasiado tonto'
+            texto.text= "Pahchin: EH?! No sé nada sobre esa tontería porque soy demasiado tonto";
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
-            text.text= 'Smiley: Si vas a pelear solo, yo haré lo mismo'
+            texto.text= "Smiley: Si vas a pelear solo, yo haré lo mismo";
         }
     }
 
